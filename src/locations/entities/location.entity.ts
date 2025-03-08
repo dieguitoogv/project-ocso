82c819e3b7031e3df29402b3bd8782d74214bf71
dieguitoogv <dieguitoogv@gmail.com>
2025-03-08 16:25:32 -0600

    Initial config for locations route

--- src/locations/entities/location.entity.ts
+++ src/locations/entities/location.entity.ts
@@ -1,1 +1,14 @@
-export class Location {}
+import { Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+
+@Entity()
+export class Location {
+    @PrimaryGeneratedColumn('increment')
+    locationId: number;
+    @Column('text')
+    locationName: string;
+    @Column('text')
+    locationAdress: string;
+    @Column('array')
+    locationLatLng: number[];
+    
+}
