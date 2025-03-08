b3c959440daf04b8624d89de24c1ebb5f1b46ea5
dieguitoogv <dieguitoogv@gmail.com>
2025-03-08 15:34:27 -0600

    Better get methods

--- src/providers/providers.service.ts
+++ src/providers/providers.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProviderDto } from './dto/create-provider.dto';
 import { UpdateProviderDto } from './dto/update-provider.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Provider } from './entities/provider.entity';
-import { Repository } from 'typeorm';
+import { Repository, Like } from 'typeorm';
+import { Product } from 'src/products/entities/product.entity';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class ProvidersService {
@@ -16,18 +18,35 @@ export class ProvidersService {
   }
 
   findAll() {
-    return `This action returns all providers`;
+    return this.providerRepository.find()
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} provider`;
+  findOne(id: string) {
+    return this.providerRepository.findOneBy({
+      providerId: id
+    })
   }
 
-  update(id: number, updateProviderDto: UpdateProviderDto) {
-    return `This action updates a #${id} provider`;
+  async findOneByName(name: string){
+    const provider = await this.providerRepository.findBy({
+      providerName: Like(`%${name}%`)
+    })
+    if (!provider) throw new NotFoundException()
+    return provider;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} provider`;
+  async update(id: string, updateProviderDto: UpdateProviderDto) {
+    const product = await this.providerRepository.preload({
+      providerId: id,
+      ...updateProviderDto
+    })
+    if (!product)throw new Error(`Provider with ID ${id} not found`);
+    return this.providerRepository.save(product);
+  }
+
+  remove(id: string) {
+    this.providerRepository.delete({
+      providerId: id
+    })
   }
 }
