4a7954d66894755a534c370beec1cd01d08f0c8a
dieguitoogv <dieguitoogv@gmail.com>
2025-03-08 23:07:33 -0600

    Auth decorator implemented

--- src/providers/providers.controller.ts
+++ src/providers/providers.controller.ts
@@ -7,8 +7,8 @@ import { UserData } from 'src/auth/decorators/user.decorator';
 import { User } from 'src/auth/entities/user.entity';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Auth } from 'src/auth/decorators/auth.decorator';
 
-@UseGuards(AuthGuard)
 @Controller('providers')
 export class ProvidersController {
   constructor(private readonly providersService: ProvidersService) {}
@@ -18,9 +18,8 @@ export class ProvidersController {
     return this.providersService.create(createProviderDto);
   }
 
-
-  @Roles(["Admin"])
-  @UseGuards(RolesGuard)
+  @Auth("Admin")
+  
   @Get()
   findAll(@UserData() user: User) {
     if(user.userRoles.includes("Employee")) throw new UnauthorizedException("No estas autorizado, solo admins y managers");
